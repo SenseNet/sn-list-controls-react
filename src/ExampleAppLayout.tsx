ec3cb70cec690425073703f91b5d2fccb7a60d6f
gallayl <gallay.lajos@gmail.com>
2018-06-04 15:53:28 +0200

    [KFI]build(project): initial project structure

---
+++ src/ExampleAppLayout.tsx
@@ -0,0 +1,15 @@
+import React = require('react')
+
+/**
+ * The Example application main layout
+ */
+export class ExampleAppLayout extends React.Component {
+    /**
+     * renders the component
+     */
+    public render() {
+        return (<div style={{ height: '100%' }}>
+            Example Application E
+        </div>)
+    }
+}
---
+++ src/example.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { ExampleAppLayout } from './ExampleAppLayout'
+import './style'
+
+ReactDOM.render(
+    <ExampleAppLayout />,
+    document.getElementById('example'),
+)
